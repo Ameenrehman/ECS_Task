cefa0ec915b46cd61b111f9d4e96ea5ae67d34f2
Ameenrehman <ameen2607@gmail.com>
2025-05-26 13:59:56 +0530

    removed path based for node app

--- terraform/main.tf
+++ terraform/main.tf
@@ -167,7 +167,7 @@ resource "aws_lb_target_group" "node_app1" {
   vpc_id      = "vpc-03323aabb25aa6abd"
   target_type = "ip"
   health_check {
-    path                = "/app1"
+    path                = "/"
     protocol            = "HTTP"
     matcher             = "200"
     interval            = 30
@@ -177,33 +177,14 @@ resource "aws_lb_target_group" "node_app1" {
   }
 }
 
-# ALB Listener with Path-Based Routing
+# ALB Listener
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
   default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Not Found"
-      status_code  = "404"
-    }
-  }
-}
-
-# Listener Rule for node-app1
-resource "aws_lb_listener_rule" "node_app1" {
-  listener_arn = aws_lb_listener.main.arn
-  priority     = 100
-  action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.node_app1.arn
-  }
-  condition {
-    path_pattern {
-      values = ["/app1", "/app1/*"]
-    }
   }
 }
 
